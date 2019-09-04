231f48ea7bfacc5c537fdaaccf069971a1b59fd4
Alex <muhmundr@gmail.com>
2019-09-04 17:19:25 +0300

    Review section

--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import SliderHero from '../components/slider-hero';
+import ReviewHero from '../components/review-hero';
 
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         <SliderHero/>
+        <ReviewHero/>
       </div>
     );
   }
