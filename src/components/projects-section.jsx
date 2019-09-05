e919d932f5ac8031392c52bccdb8914284749be4
Alex <muhmundr@gmail.com>
2019-09-05 14:14:17 +0300

    Promo blocks

--- src/components/projects-section.jsx
+++ src/components/projects-section.jsx
@@ -27,58 +27,56 @@ class ProjectsSection extends React.Component {
   render() {
     return (
       <div className="projects-section">
-        <div className="wrapper-1180">
-          <h2 className="additional-header h1"> Professional Installation Services</h2>
-          <PaneSwitcher>
-            <div className="projects-section__project-cards" data-name="Fence projects">
-              {this.renderCard({
-                img: photoNailUpProject,
-                title: 'Nail Up',
-                content: 'This fence features non-overlapping 6 ft high ' +
+        <h2 className="additional-header h1"> Professional Installation Services</h2>
+        <PaneSwitcher>
+          <div className="projects-section__project-cards" data-name="Fence projects">
+            {this.renderCard({
+              img: photoNailUpProject,
+              title: 'Nail Up',
+              content: 'This fence features non-overlapping 6 ft high ' +
                     'boards with curved tops on one side and the frame on the other side.',
-                link: 'https://app.ergeon.com/projects-gallery/#/tags/nail-up',
-              })}
-              {this.renderCard({
-                img: photoPictureFrame,
-                title: 'Picture Frame',
-                content: 'This fence features overlapping boards for added durability, ' +
+              link: 'https://app.ergeon.com/projects-gallery/#/tags/nail-up',
+            })}
+            {this.renderCard({
+              img: photoPictureFrame,
+              title: 'Picture Frame',
+              content: 'This fence features overlapping boards for added durability, ' +
                     'and each side of the fence looks identical. This is ideal for fences on shared boundary lines.',
-                link: 'https://app.ergeon.com/projects-gallery/#/tags/picture-frame',
-              })}
-              {this.renderCard({
-                img: photoPictureFrameLattice,
-                title: 'Picture Frame with Lattice',
-                content: 'This fence is the same as our Picture Frame Fence, ' +
+              link: 'https://app.ergeon.com/projects-gallery/#/tags/picture-frame',
+            })}
+            {this.renderCard({
+              img: photoPictureFrameLattice,
+              title: 'Picture Frame with Lattice',
+              content: 'This fence is the same as our Picture Frame Fence, ' +
                     'with a lattice on top for added height and privacy.',
-                link: 'https://app.ergeon.com/projects-gallery/#/tags/lattice',
-              })}
-            </div>
-            <div className="projects-section__project-cards" data-name="Driveway projects">
-              {this.renderCard({
-                img: photoBrushed,
-                title: 'Brushed Concrete',
-                content: 'Brushed Concrete This style of paving features poured concrete finished by pulling ' +
+              link: 'https://app.ergeon.com/projects-gallery/#/tags/lattice',
+            })}
+          </div>
+          <div className="projects-section__project-cards" data-name="Driveway projects">
+            {this.renderCard({
+              img: photoBrushed,
+              title: 'Brushed Concrete',
+              content: 'Brushed Concrete This style of paving features poured concrete finished by pulling ' +
                     'a brush over the fresh concrete surface. Brushed Concrete is suitable for high vehicular ' +
                     'and foot traffic areas.',
-                link: '',
-              })}
-              {this.renderCard({
-                img: photoStamped,
-                title: 'Stamped Concrete',
-                content: 'This style of paving features poured concrete, then patterned stamped on top of the fresh ' +
+              link: '',
+            })}
+            {this.renderCard({
+              img: photoStamped,
+              title: 'Stamped Concrete',
+              content: 'This style of paving features poured concrete, then patterned stamped on top of the fresh ' +
                     'concrete surface. This finish will create patterns and textures that looks like natural stone.',
-                link: '',
-              })}
-              {this.renderCard({
-                img: photoPavers,
-                title: 'Pavers',
-                content: 'This style of paving features precast concrete and natural stone pavers that can come in ' +
+              link: '',
+            })}
+            {this.renderCard({
+              img: photoPavers,
+              title: 'Pavers',
+              content: 'This style of paving features precast concrete and natural stone pavers that can come in ' +
                     'various modular sizes and shapes. This finish will create natural and geometric patterns.',
-                link: '',
-              })}
-            </div>
-          </PaneSwitcher>
-        </div>
+              link: '',
+            })}
+          </div>
+        </PaneSwitcher>
       </div>
     );
   }
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -2,14 +2,31 @@ import React from 'react';
 import SliderHero from '../components/slider-hero';
 import ReviewHero from '../components/review-hero';
 import ProjectsSection from '../components/projects-section';
-
+import PromoBlock from '../components/promo-block';
+import imgCalc from '../assets/calc_promo.png';
+import imgMap from '../assets/map_image.png';
 class HomePage extends React.Component {
   render() {
     return (
       <div>
         <SliderHero/>
         <ReviewHero/>
-        <ProjectsSection/>
+        <div className="wrapper-1180">
+          <ProjectsSection/>
+          <PromoBlock
+            btnLink="https://fencequoting.com/"
+            btnName="Fence Calculator"
+            img={imgCalc}
+            subtitle="Build your dream fence and get an estimate instantly"
+            title="Try Our Fence Calculator"/>
+          <PromoBlock
+            btnLink="https://app.ergeon.com/projects-gallery/#/map"
+            btnName="Open Map"
+            img={imgMap}
+            subtitle="Find fence projects near your house"
+            title="Browse Projects In Your Area"/>
+        </div>
+
       </div>
     );
   }
