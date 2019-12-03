81e360bdd9fa47b6d5c8c07827957608deef5304
Alexey Evseev <myhappydo@gmail.com>
2019-12-03 16:55:52 +0300

    [ENG-1694] Remove unused API (#62)

--- src/libs/api.js
+++ src/libs/api.js
@@ -1,42 +1,6 @@
 import config from 'libs/config';
 import axios from 'axios';
 
-export const submitNoAddressLeadData = (data) => {
-  return axios({
-    url: `${config.myErgeonURL}/c/api/v1/leads/call/`,
-    method: 'post',
-    data: JSON.stringify(data),
-    responseType: 'json',
-    headers: {'Content-Type': 'application/json'},
-  });
-};
-export const submitLeadData = (data, lead) => {
-  return axios({
-    url: `${config.myErgeonURL}/c/api/v1/quotes/`,
-    method: 'post',
-    data: JSON.stringify(data),
-    responseType: 'json',
-    headers: {'Content-Type': 'application/json', 'Authorization': `LeadToken ${lead.token}`},
-  });
-};
-export const submitWaitListLeadData = (data, lead) => {
-  return axios({
-    url: `${config.myErgeonURL}/c/api/v1/lead/${lead.gid}/`,
-    method: 'patch',
-    data: JSON.stringify(data),
-    responseType: 'json',
-    headers: {'Content-Type': 'application/json'},
-  });
-};
-export const submitAddressData = (data) => {
-  return axios({
-    url: `${config.myErgeonURL}/c/api/v1/lead/`,
-    method: 'post',
-    data: JSON.stringify(data),
-    responseType: 'json',
-    headers: {'Content-Type': 'application/json'},
-  });
-};
 export const submitLeadArrived = (data) => {
   return axios({
     url: config.leadArrivedApi,
