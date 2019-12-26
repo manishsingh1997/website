40a9f3b5a6327864aa3c324d6d491381688cd511
Alexey Evseev <myhappydo@gmail.com>
2019-12-27 01:04:35 +0700

    [ENG-1707] Implement customer menus for mobile (#106)

--- src/components/AppLayout/index.jsx
+++ src/components/AppLayout/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {NavLink} from 'react-router-dom';
 import {ReactSVG} from 'react-svg';
+import classNames from 'classnames';
 
 import {NavLinkContext} from '@ergeon/core-components';
 
@@ -16,36 +17,46 @@ export default class AppLayout extends React.Component {
     match: PropTypes.object,
   };
 
-  renderMenus() {
+  state = {
+    expandedSidebar: false,
+  };
+
+  renderSideBar() {
     const {match} = this.props;
+    const {expandedSidebar} = this.state;
 
     return (
       <NavLinkContext.Provider value={NavLink}>
-        <ul className="siblings-list">
-          {getMenuItems(match.url).map(
-            (menuItem => (
-              <NavLink activeClassName="active-link" key={`app-menu-${menuItem.title}`} to={menuItem.path}>
-                <li className="siblings-list-item">
-                  <div className="menu-icon-wrapper">
-                    <ReactSVG src={menuItem.iconSVG} />
-                  </div>
-                  <div className="siblings-list-item__wrapper">{menuItem.title}</div>
-                </li>
-              </NavLink>
-            ))
-          )}
-        </ul>
+        <div className={classNames({'sidebar--expanded': expandedSidebar})} >
+          <div className="sidebar-title">
+            Menu
+            <i className="icon-arrow-down" onClick={() => this.setState({expandedSidebar: !expandedSidebar})} />
+          </div>
+          <ul className="siblings-list">
+            {getMenuItems(match.url).map(
+              (menuItem => (
+                <NavLink activeClassName="active-link" key={`app-menu-${menuItem.title}`} to={menuItem.path}>
+                  <li className="siblings-list-item">
+                    <div className="menu-icon-wrapper">
+                      <ReactSVG src={menuItem.iconSVG} />
+                    </div>
+                    <div className="siblings-list-item__wrapper">{menuItem.title}</div>
+                  </li>
+                </NavLink>
+              ))
+            )}
+          </ul>
+        </div>
       </NavLinkContext.Provider>
     );
   }
 
   render() {
-
     return (
       <div className="customer-app-layout">
-        <div className="flex-cards">
-          <div className="customer-app-menu card shadow soft-border">
-            {this.renderMenus()}
+        <div className="cards-wrapper">
+          <div className="customer-app-sidebar card shadow soft-border">
+            {this.renderSideBar()}
           </div>
           <div className="customer-app-content card shadow soft-border">
             {this.props.children}
