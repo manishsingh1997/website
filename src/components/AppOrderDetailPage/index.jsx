9a25e46af7fbe04253d93386b4d16e4bb2505c40
Alexey Evseev <myhappydo@gmail.com>
2020-01-27 21:43:46 +0700

    [ENG-2062] Add quote preview to order detail page (#166)

--- src/components/AppOrderDetailPage/index.jsx
+++ src/components/AppOrderDetailPage/index.jsx
@@ -4,8 +4,9 @@ import {Link} from 'react-router-dom';
 import {ReactSVG} from 'react-svg';
 import Select from 'react-select';
 
-import {Button} from '@ergeon/core-components';
+import {Button, Spinner} from '@ergeon/core-components';
 import contactIcon from '@ergeon/core-components/src/assets/icon-arrow-left.svg';
+import {calcUtils} from '@ergeon/3d-lib';
 
 import {formatDate, formatDateAndTime} from 'utils/date';
 import {filterQuotesSentToCustomer, formatPrice, QUOTE_FILTERS} from 'utils/app-order';
@@ -31,6 +32,7 @@ export default class AppOrderDetailPage extends React.Component {
 
   state = {
     selectedOption: QUOTE_FILTERS[0],
+    previewImages: {},
   };
 
   static contextType = CustomerGIDContext;
@@ -58,6 +60,23 @@ export default class AppOrderDetailPage extends React.Component {
     this.setState({selectedOption});
   };
 
+  fetchQuotePreview(quote) {
+    const data = calcUtils.getValueFromUrl(`/?${quote['preview_quote_line']['config']['schema_code_url']}`);
+    calcUtils.getPreviewImage(data).then(
+      preview => {
+        this.setState(prevState => (
+          {previewImages: {...prevState.previewImages, [quote['id']]: preview}})
+        );
+      }
+    ).catch(error => {
+      // save `null` in case of error, so preview image will not be shown at all
+      this.setState(prevState => (
+        {previewImages: {...prevState.previewImages, [quote['id']]: null}})
+      );
+      throw error;  // re-raise an error so we'll be notified
+    });
+  }
+
   renderListElementHeader(quote) {
     return (
       <React.Fragment>
@@ -69,20 +88,44 @@ export default class AppOrderDetailPage extends React.Component {
   renderListElementContent(quote) {
     const customerGID = this.context;
 
+    const schemaCodeUrl = quote['preview_quote_line'] && quote['preview_quote_line']['config']['schema_code_url'];
+    const previewImage = schemaCodeUrl ? this.state.previewImages[quote['id']]: null;
+    let quotePreviewComponent = null;
+
+    if (previewImage === undefined) {
+      this.fetchQuotePreview(quote);
+      quotePreviewComponent = <Spinner active={true} borderWidth={.15} color="green" size={64} />;
+    } else if (previewImage) {
+      quotePreviewComponent = <img src={previewImage} />;
+    }
+
     return (
       <React.Fragment>
-        <DataRow title="Status" value={quote['status_display']} />
-        <DataRow title="Total Price" value={formatPrice(quote['total_price'])} />
-        <DataRow title="Sent At" value={formatDateAndTime(quote['sent_to_customer_at'])} />
-        {quote['approved_at'] && <DataRow title="Approved At" value={formatDateAndTime(quote['approved_at'])} />}
-        {quote['cancelled_at'] && <DataRow title="Cancelled At" value={formatDateAndTime(quote['cancelled_at'])} />}
-        <Link to={getQuoteDetailURL(customerGID, quote['order_id'], quote['id'])}>
-          <Button
-            size="large"
-            type="submit">
-              View Full Quote
-          </Button>
-        </Link>
+        <div className="flex-wrapper">
+          <div>
+            <DataRow title="Status" value={quote['status_display']} />
+            <DataRow title="Total Price" value={formatPrice(quote['total_price'])} />
+            <DataRow title="Sent At" value={formatDateAndTime(quote['sent_to_customer_at'])} />
+            {quote['approved_at'] && <DataRow title="Approved At" value={formatDateAndTime(quote['approved_at'])} />}
+            {quote['cancelled_at'] && <DataRow title="Cancelled At" value={formatDateAndTime(quote['cancelled_at'])} />}
+          </div>
+          <div>
+            {previewImage !== null && (
+              <div className="quote-line-preview border">
+                {quotePreviewComponent}
+              </div>
+            )}
+          </div>
+        </div>
+        <div>
+          <Link className="spacing left__is-10" to={getQuoteDetailURL(customerGID, quote['order_id'], quote['id'])}>
+            <Button
+              size="large"
+              type="submit">
+                View Full Quote
+            </Button>
+          </Link>
+        </div>
       </React.Fragment>
     );
   }
