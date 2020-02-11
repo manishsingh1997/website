77c9e83d23a34a8a77390c50953fc2d36a6ccbb6
Alexey Degtyarik <alexd@ergeon.com>
2020-02-12 00:54:48 +0900

    Fix default fence model (#187)

--- src/components/RequestQuotePage/InlineEditor.jsx
+++ src/components/RequestQuotePage/InlineEditor.jsx
@@ -5,8 +5,8 @@ import {Viewer, calcUtils} from '@ergeon/3d-lib';
 
 import {getPriceAndDescription} from 'api/lead';
 
-const defaultModel = '?schema=3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,32' +
-  '&code=F6,NU,SL8,BB,FT,PK8,RH,POS,PTS,PTM,PD2,PH8,PRZ,KPT,1K12G,R2,RRC24,LZ,L0,RW0,CZS';
+const defaultModel = '?schema=3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,32&' +
+  'code=F6,NU,SL8,SS,FT,PK8,RC,POS,PT4,PPT,PD2,PH8,PRZ,KZ,K0,R2,RRC24,LZ,L0,RW0,CZS';
 
 export default class InlineEditor extends React.Component {
   static propTypes = {
