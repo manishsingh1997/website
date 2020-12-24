66bdd14992cdf1525eddae9435bc7568379591c8
Vlad Shulyugin <vladislav.shulyugin@gmail.com>
2020-12-24 18:40:11 +0300

    [ENG-5045] Add noindex to error page (#674)

--- src/components/NotFoundPage/index.jsx
+++ src/components/NotFoundPage/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {Helmet} from 'react-helmet';
 
 import errorPage from '@ergeon/core-components/src/assets/error-page.svg';
 import ErrorPage from '../common/ErrorPage';
 
 const NotFoundPage = () => (
-  <ErrorPage image={errorPage} title="Not Found"></ErrorPage>
+  <>
+    <Helmet>
+      <meta content="noindex" name="robots" />
+    </Helmet>
+    <ErrorPage image={errorPage} title="Not Found"></ErrorPage>
+  </>
 );
 
 export default NotFoundPage;
