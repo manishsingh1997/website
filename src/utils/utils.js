6c5586c2152dcfbf4528e894584c009d8058e234
Shynggys <kschingiz@gmail.com>
2021-02-05 18:21:12 +0600

    [ENG-6085] Add promise timeout functions (#717)
    * [ENG-6085] Add promise timeout functions
    
    * add newline

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -91,3 +91,31 @@ export const scrollTop = () => {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 };
+
+/**
+ * Rejects if specified promise doesn’t resolve in provided `timeout` ms.
+ * @param {Promise} promise
+ * @param {string} funcName
+ * @param {number} timeout ms
+ */
+export const rejectOnTimeout = function(promise, funcName, timeout = 500) {
+  const timeoutPromise = new Promise((resolve, reject) => {
+    const timeoutID = setTimeout(() => {
+      clearTimeout(timeoutID);
+      reject(`Function ${funcName} took more than ${timeout} ms to execute, rejecting`);
+    }, timeout);
+  });
+  return Promise.race([promise, timeoutPromise]);
+};
+
+/**
+ * Resolve if specified promise doesn’t resolve in provided `timeout` ms.
+ * @param {Function} fn
+ * @param {string} funcName
+ * @param {number} timeout ms
+ */
+export const resolveOnTimeout = function(promise, funcName, timeout = 500) {
+  return new Promise((resolve) => {
+    rejectOnTimeout(promise, funcName, timeout).finally(resolve);
+  });
+};
