8a1e0239cc48f95974bd5d2cbad287c87906e556
vladsafedev <88082897+vladsafedev@users.noreply.github.com>
2021-08-31 15:13:32 +0300

    [ENG-5514] Remove help pages meta (#1044)
    Help pages are able to provide meta now, it is no longer needed inside the website, removing redundant code.

--- src/components/common/MetaTags.jsx
+++ src/components/common/MetaTags.jsx
@@ -1,17 +1,13 @@
-import React, {useCallback, useMemo} from 'react';
+import React, {useCallback} from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import {useRouteMatch} from 'react-router-dom';
 import compact from 'lodash/compact';
 import get from 'lodash/get';
-import isNil from 'lodash/isNil';
 
 import {MetaTags} from '@ergeon/core-components';
 
-import {getNodes} from '../../api/node';
 import metaDictionary from '../../data/meta-data.json';
 import config from '../../website/config';
-import {helpNodePath} from '../../routes/public';
 import {logDev} from '../../utils/log';
 
 /**
@@ -20,11 +16,6 @@ import {logDev} from '../../utils/log';
  */
 const MetaTagsConnected = ({auth = {}}) => {
   const {user} = auth;
-
-  const match = useRouteMatch(helpNodePath);
-
-  // Get help node’s key if the URL is matching `helpNodePath`.
-  const helpNodeKey = useMemo(() => match?.params?.nodeKey, [match]);
 
   /**
    * Get meta-data
@@ -39,23 +30,6 @@ const MetaTagsConnected = ({auth = {}}) => {
 
     if (metaDictionary[path]) {
       meta = {...meta, ...metaDictionary[path]};
-    } else if (helpNodeKey) {
-      let helpMeta = metaDictionary['/help'];
-      try {
-        // Retrieve help meta-data
-        const {data: [helpNode]} = await getNodes([helpNodeKey]);
-        if (!isNil(helpNode)) {
-          helpMeta = {
-            title: `${helpNode.title} | ${helpMeta.title}`,
-            description: helpNode.short_memo,
-          };
-        }
-      } catch (error) {
-        // Just warn Sentry in case of an error, do not interfere
-        console.error(error.message);
-      } finally {
-        meta = {...meta, ...helpMeta};
-      }
     } else if (isPrivateArea) {
       const getPathAfterUserGIDRegex = new RegExp(/^\/app\/[\w\-]{16}\/(.+)/i);
       const subPath = get(path.match(getPathAfterUserGIDRegex), 1);
@@ -80,7 +54,7 @@ const MetaTagsConnected = ({auth = {}}) => {
     logDev('META', JSON.stringify(meta, null, 2));
 
     return meta;
-  }, [helpNodeKey, user]);
+  }, [user]);
 
   return <MetaTags {...{getMeta}} />;
 };
