de6b10bf32d58d660ad67c73ca7352d7aaf7ee17
Luis <luismexin@me.com>
2021-09-06 07:25:08 -0500

    [ENG-9006] Add google and yelp reviews with a/b testing (#1041)
    * [ENG-9006] Add google and yelp reviews with a/b testing
    
    * Remove experimentDebugger
    
    * Apply review comments
    
    * Change json to hashed list
    
    * Add review-logo-height comment

---
+++ src/components/HomePage/ReviewExperiment.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {Rating} from '@ergeon/core-components';
+import classNames from 'classnames';
+import config from '../../website/config';
+
+type ReviewExperimentTypes = {
+  logo: string,
+  rating: number,
+  reviewCount: string,
+  reviewerName: string,
+  text: string,
+  title: string,
+  url: string,
+  classLogo: string,
+}
+
+const ReviewExperiment = (props: ReviewExperimentTypes) => {
+  const {url, rating, reviewCount, logo, title, text, reviewerName, classLogo} = props;
+  return (
+    <div className="review-hero__flex-wrapper">
+      <div className="review-hero__customer-reviews">
+        <h4>Customer reviews</h4>
+        <a href={url}>
+          <div className="review-hero__customer-reviews__home-advisor-section">
+            <div className="review-hero__customer-reviews__rating">
+              <Rating value={rating > 4.5 ? 4.5 : rating} />
+              <div className="review-hero__customer-reviews__rating__numbers">
+                {rating} / {reviewCount} Verified Reviews</div>
+            </div>
+            <img className={classNames(classLogo)} src={logo} />
+          </div>
+        </a>
+
+      </div>
+      <div className="review-hero__main-review">
+        <h4>{title}</h4>
+        <p><i>
+          {text}
+        </i></p>
+        <div className="review-hero__main-review__bottom">
+          <span className="label">{reviewerName}</span>
+          <span>
+            More reviews on <a href={`${config.projectsGalleryHost}/tags/feedback`}>Project Showcase</a>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewExperiment;
---
+++ src/components/HomePage/ReviewHero.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import {Experiment, Variant} from '@marvelapp/react-ab-test';
+import {RatingsData} from '@ergeon/core-components';
+import omit from 'lodash/omit';
+
+import homeAdvisorLogo from '../../assets/home_advisor@2x.png';
+import googleLogo from '../../assets/google@2x.png';
+import yelpLogo from '../../assets/yelp@2x.png';
+
+import reviews from './reviews.json';
+import ReviewExperiment from './ReviewExperiment';
+
+import './ReviewHero.scss';
+
+/*
+ * ReviewHero should display experiments based on customer reviews
+ * Now that we have A/B testing in order to check variants locally we need to add
+ * experimentDebugger.enable(); (include import {experimentDebugger} on react-ab-test)
+ */
+const ReviewHero = () => {
+  const {homeadvisor, google, yelp} = RatingsData;
+  const reviewsArray = [
+    {
+      ...reviews.homeadvisor,
+      ...homeadvisor,
+      logo: homeAdvisorLogo,
+    },
+    {
+      ...reviews.google,
+      ...google,
+      logo: googleLogo,
+    },
+    {
+      ...reviews.yelp,
+      ...yelp,
+      logo: yelpLogo,
+    },
+  ];
+
+  return (
+    <div className="review-hero">
+      <div className="wrapper-1180">
+        <Experiment name="customerReviewsExperiment">
+          {reviewsArray.map((review, i) => (
+            <Variant key={`variant${i}`} name={review.variant}>
+              <ReviewExperiment {...omit(review, ['variant'])} />
+            </Variant>
+          ))}
+        </Experiment>
+      </div>
+    </div>
+  );
+};
+
+export default ReviewHero;
