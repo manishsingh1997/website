c9643ce3d81e8e641f6767abe32ddbca94e0e1fb
Luis <mexinluis@gmail.com>
2022-03-04 13:33:12 -0600

    [ENG-12801] Fix invalid PropType check for isMobileWidth (#1314)

--- src/components/common/AppConfigPreview.jsx
+++ src/components/common/AppConfigPreview.jsx
@@ -25,7 +25,7 @@ export default class AppConfigPreview extends React.Component {
     configType: PropTypes.string,
     fenceSideLength: PropTypes.number,
     images: PropTypes.array,
-    isMobileWidth: PropTypes.oneOf([PropTypes.boolean, undefined]),
+    isMobileWidth: PropTypes.bool,
     propertySchemaCodeUrl: PropTypes.string,
     schemaCodeUrl: PropTypes.string,
     useNoPreviewIcon: PropTypes.bool,
