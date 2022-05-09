d5e02baf3cb83490aea0743cff8bd01df332aea8
Dmitry Degtyarik <71075560+iloput@users.noreply.github.com>
2022-05-09 19:33:12 +0300

    [ENG-13774] Test SignIn Page (#1484)

---
+++ src/components/AuthSignInPage/__tests__/AuthSignInPage.tests.tsx
@@ -0,0 +1,54 @@
+import {BrowserRouter} from 'react-router-dom';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import React from 'react';
+
+import AuthSignInPage from '../index';
+
+import '@testing-library/jest-dom';
+
+jest.mock('../../../components/common/Success');
+
+jest.mock('utils/auth', () => ({
+  ...jest.requireActual('utils/auth'),
+  authService: {requestOTP: jest.fn()},
+}));
+
+describe('Rendering of Auth Sign In Page', () => {
+  it('should render success', async () => {
+    render(
+      <BrowserRouter>
+        <AuthSignInPage />
+      </BrowserRouter>
+    );
+
+    const submitButton = screen.getByText('Sign in with email');
+    const input = screen.getByLabelText('Email');
+    fireEvent.change(input, {target: {value: 'test@ergeon.com'}});
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const success = screen.getByText('We have sent you an email with a link to sign in');
+      expect(success).toBeInTheDocument();
+    });
+  });
+
+  it('should render SignIn form', () => {
+    render(
+      <BrowserRouter>
+        <AuthSignInPage />
+      </BrowserRouter>
+    );
+
+    const signInText = screen.getByText('Sign in to Ergeon');
+    const quoteLink = screen.getByText('Request a quote');
+    const emailInput = screen.getByLabelText('Email');
+    const signInButton = screen.getByText('Sign in with email');
+    const footerText = screen.getByText('We will send you an email with a link for sign in');
+
+    expect(signInText).toBeInTheDocument();
+    expect(quoteLink).toBeInTheDocument();
+    expect(emailInput).toBeInTheDocument();
+    expect(signInButton).toBeInTheDocument();
+    expect(footerText).toBeInTheDocument();
+  });
+});
---
+++ src/components/AuthSignInPage/__tests__/AuthSignInPageError.tests.tsx
@@ -0,0 +1,38 @@
+import {BrowserRouter} from 'react-router-dom';
+import {fireEvent, render, screen, waitFor} from '@testing-library/react';
+import React from 'react';
+
+import AuthSignInPage from '../index';
+
+import '@testing-library/jest-dom';
+
+jest.mock('../../../components/common/Success');
+
+jest.mock('utils/auth', () => ({
+  ...jest.requireActual('utils/auth'),
+  authService: {
+    requestOTP: jest.fn(() => {
+      throw new Error();
+    }),
+  },
+}));
+
+describe('Rendering of Auth Sign In Page', () => {
+  it('should show error', async () => {
+    render(
+      <BrowserRouter>
+        <AuthSignInPage />
+      </BrowserRouter>
+    );
+
+    const submitButton = screen.getByText('Sign in with email');
+    const input = screen.getByLabelText('Email');
+    fireEvent.change(input, {target: {value: 'test@ergeon.com'}});
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const error = screen.getByText('Unexpected error, we are already notified about this.');
+      expect(error).toBeInTheDocument();
+    });
+  });
+});
