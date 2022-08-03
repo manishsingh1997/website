a9f57b35cec76d63c2996c872163eda3e02f66ea
Alexandro Tapia <106361809+zpaceway@users.noreply.github.com>
2022-08-03 08:45:49 -0500

    [ENG-14493] Use paginated view with MapView API (#1736)
    * Refactor src/api/map.js to use paginated view
    
    Co-authored-by: Alexandro Tapia <alexandrotapiaflores@gmail.com>

--- src/api/map.js
+++ src/api/map.js
@@ -1,11 +1,54 @@
 import {ensureUpcomingFeaturesParamInUrl} from '@ergeon/erg-utils-js';
 import axios from 'axios';
 
-export const getMapData = (gid) => {
-  return axios({
+export const getMapData = async (gid) => {
+  const mapDataResponse = await axios({
     method: 'get',
-    url: ensureUpcomingFeaturesParamInUrl(`${process.env.API_HOST}/api/geo/map/${gid}/`),
+    url: ensureUpcomingFeaturesParamInUrl(
+      `${process.env.API_HOST}/api/geo/map/paginated/${gid}/`
+    ),
     responseType: 'json',
     headers: {'Content-Type': 'application/json'},
   });
+
+  const firstPage = mapDataResponse.data;
+
+  const { markers: markersPages, polygons: polygonsPages } = firstPage.total_pages;
+  const totalPages = markersPages > polygonsPages ? markersPages : polygonsPages;
+
+  const markers = [];
+  const polygons = [];
+
+  const { markers: markersInPage, polygons: polygonsInPage } = mapDataResponse.data;
+  markers.push(markersInPage);
+  polygons.push(polygonsInPage);
+
+  await Promise.all(
+    Array.from({
+      length: (totalPages - 1) > 0 ? totalPages - 1 : 0
+    }).map(async (_, index) => {
+      const pageNumber = index + 2
+      const { data } = await axios({
+        method: 'get',
+        url: 
+          ensureUpcomingFeaturesParamInUrl(
+            `${
+              process.env.API_HOST
+            }/api/geo/map/paginated/${gid}/?page_number=${
+              pageNumber
+            }`
+          ),
+        responseType: 'json',
+        headers: {'Content-Type': 'application/json'},
+      });
+      const { markers: markersInPage, polygons: polygonsInPage } = data;
+      markers.push(markersInPage);
+      polygons.push(polygonsInPage);
+    })
+  );
+
+  mapDataResponse.data.markers = markers.flat();
+  mapDataResponse.data.polygons = polygons.flat();
+
+  return mapDataResponse;
 };
